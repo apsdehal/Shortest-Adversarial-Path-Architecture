a1180a9f815bfb6e2ad40f654b8f84bc53a5e9a0
Amanpreet Singh <apsdehal@gmail.com>
2016-10-04 05:43:16 -0400

    Add test clients

---
+++ test-clients/test-player.py
@@ -0,0 +1,33 @@
+# Echo client program
+import socket
+import time
+
+HOST = 'localhost'    # The remote host
+PORT = 5000       # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+flag = 0
+
+allData = ""
+count = 0
+while(1):
+    data = s.recv(1024)
+    if "#" not in data and flag == 0:
+        allData += data
+        continue
+
+    flag = 1
+
+    if "#" in data:
+        allData = [x.strip() for x in allData.split("\n")];
+        allData = allData[3:]
+
+    if "$" in data:
+        break
+
+    curr = allData[count]
+    count += 1
+    print "Selecting " + curr.split(" ")[1]
+    s.sendall('{}'.format(curr.split(" ")[1]))
+s.close()
