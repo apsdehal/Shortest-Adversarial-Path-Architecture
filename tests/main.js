0a33c4b7ce03f9d48c206549fa39f4d2704d43e6
Amanpreet Singh <apsdehal@gmail.com>
2016-10-10 19:59:28 -0400

    Add tests for player one moves

--- tests/main.js
+++ tests/main.js
@@ -1,21 +1,85 @@
 var fs = require('fs');
 var assert = require('assert');
-var gameManager = require('../src/game-manager');
 
 describe('File Reader test', function () {
   it('should parse and return correctly formatted data', function () {
     var fileData = fs.readFileSync('./advshort');
+    var gameManager = require('../src/game-manager');
 
     gameManager.initializeGame(fileData.toString());
 
-    assert.equal(gameManager.gameData.startPoint, '165');
-    assert.equal(gameManager.gameData.endPoint, '157');
-    assert.equal(gameManager.gameData.playerOnePosition, '165');
-    assert.equal(gameManager.gameData.playerBill, '0');
+    assert.strictEqual(gameManager.gameData.startPoint, '165');
+    assert.strictEqual(gameManager.gameData.endPoint, '157');
+    assert.strictEqual(gameManager.gameData.playerOnePosition, '165');
+    assert.strictEqual(gameManager.gameData.playerBill, '0');
     assert.deepEqual(gameManager.gameData.graph.adjacencyList['165'],
     [ '84', '163', '73', '149', '143', '153' ]);
     assert.deepEqual(gameManager.gameData.graph.adjacencyList['153'],
     [ '25', '168', '165', '159', '75', '26', '108', '76' ]);
 
   });
+
+  it('should properly handle player one moves', function () {
+    var fileData = fs.readFileSync('./advshort');
+    var gameManager = require('../src/game-manager');
+
+    gameManager.initializeGame(fileData.toString());
+
+    var connHandler = {
+      notifyPlayerTwo: function (data) {
+        assert.strictEqual(data, '84');
+      }
+    }
+
+    // Valid Move handling
+    gameManager.playerOneMove('84', connHandler);
+    assert.strictEqual(gameManager.gameData.playerOnePosition, '84');
+
+    gameManager.playerOneMove('84 64', connHandler);
+    assert.strictEqual(gameManager.gameData.playerOnePosition, '84');
+
+    gameManager.playerOneMove('84 ', connHandler);
+    assert.strictEqual(gameManager.gameData.playerOnePosition, '84');
+
+    connHandler = {
+      notifyPlayerTwo: function (data) {
+        assert.strictEqual(data, '165');
+      }
+    }
+
+    gameManager.playerOneMove('165', connHandler);
+    assert.strictEqual(gameManager.gameData.playerOnePosition, '165');
+
+    gameManager.playerOneMove('165 64', connHandler);
+    assert.strictEqual(gameManager.gameData.playerOnePosition, '165');
+
+    gameManager.playerOneMove('165 ', connHandler);
+    assert.strictEqual(gameManager.gameData.playerOnePosition, '165');
+
+    connHandler = {
+      notifyPlayerTwo: function (data) {
+        assert.strictEqual(data, '165');
+      }
+    }
+
+    // Invalid move handling
+    gameManager.playerOneMove('86', connHandler);
+
+    assert.strictEqual(gameManager.gameData.playerBill, '2');
+
+    gameManager.gameData.doubleEdgeCost('165', '84');
+
+    var called = false;
+    gameManager.notifyEndGame = function () {
+      called = true;
+    }
+
+    gameManager.gameData.endPoint = '84';
+
+    gameManager.playerOneMove('84', connHandler);
+
+    assert.equal(called, true);
+
+    assert.strictEqual(gameManager.gameData.playerBill, '4');
+  })
 });
